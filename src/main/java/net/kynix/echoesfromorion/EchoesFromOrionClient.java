496027a756bca0d8d2742677cb5deee4fb8f9e70
Kynix-x <kynix-x@users.noreply.github.com>
2025-07-07 04:35:55 +0300

    first commit

---
+++ src/main/java/net/kynix/echoesfromorion/EchoesFromOrionClient.java
@@ -0,0 +1,10 @@
+package net.kynix.echoesfromorion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoesFromOrionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kynix/echoesfromorion/EchoesFromOrionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kynix.echoesfromorion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EchoesFromOrionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
